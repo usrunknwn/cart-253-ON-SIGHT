dd4cfcf4bb4d91e3551b6dcd0bb6cdf1b7bfda31
usrunknwn <116674027+usrunknwn@users.noreply.github.com>
2022-10-31 13:44:43 -0400

    Update script.js

--- template-p5-project 2 2/js/script.js
+++ template-p5-project 2 2/js/script.js
@@ -5,125 +5,126 @@ Damian Larice
 
 "use strict";
 
-let face = []
-
-let myVar = 0;
-let clickCounter = 0;
-let ale = [];
-let dle = 4;
-var gif;
-
+//VARIABLES
+let uu = []
+let shirts = []
+let back;
+let beam = []
+let loading;
+let user;
 
 function preload() {
 
+//IMAGES
+uu[0] = loadImage('assets/images/left eye.png');
+uu[1] = loadImage('assets/images/right eye.png');
+uu[2] = loadImage('assets/images/mouth.png');
 
-face[0] = loadImage('assets/images/left eye1.png');
-face[1] = loadImage('assets/images/right eye1.png');
-face[2] = loadImage('assets/images/mouth1.png');
+shirts[0] = loadImage('assets/images/shirt1253.png');
+shirts[1] = loadImage('assets/images/shirt2253.png');
+shirts[2] = loadImage('assets/images/shirt3253.png');
 
-gif = createImg('assets/images/background.gif');
+beam[0] = loadImage('assets/images/beam.png');
+
+back = createVideo(['assets/images/back.mp4','assets/images/back1.webm']);
+back.hide();
+
+loading = loadImage('assets/images/loading.png');
+user = loadImage('assets/images/user.png');
 
 frameRate(10);
-
 }
 
 
-//let sqSize = [100, 39, 18, 17]
+
+let y
 
 function setup() {
 
 createCanvas(1080, 1920);
 background(200,200,200);
 
+imageMode(CENTER);
+angleMode(DEGREES);
 
 
 
+y = 0
 
 }
+
 
 function mousePressed(){
   clickCounter++;
   print(clickCounter);
   print(mouseX,mouseY);
-
-
-
 }
 
+let count = 0;
 
-/*function draw(){
-
-  for( let i = 0; i < sqSize.length; i++){
-    fill(100,50);
-    drawSquare(sqSize[i],100);
-  }
-}
-function drawSquare(size, fColor){
-  rectMode(CENTER);
-  rect(mouseX+random(-10,10), mouseY + random(-10,10), size, size);
-}
-
-//  gif.position(1380, 32);
-  //gif.size(1080, 1920);
-//}
-
-function check(){
-  if(mouseX < width/2 && mouseY < height/2){
-      return true;
-  } else {
-    return false;
-  }
-}
-
-
-function keyPressed(){
-  sqSize.pop();
-}
-*/
 function draw() {
-  print(frameCount);
+ print(frameCount);
+ //LOADING FACE ANIMATION BEFORE
 
-  var eyeLeft = [1,19,37];
-  var eyeRight = [7,25,43];
-  var mouth = [13,31,49];
-  var reset = [6,12,18,24,30,36,42,48,54];
+  push()
+  translate(width / 5, height / 3);
+  var eyeLeft = [1,19,37,55];
+  var eyeRight = [7,25,43,61];
+  var mouth = [13,31,49,67];
+  var reset = [6,12,18,24,30,36,42,48,54,60,66,72];
 
   if(eyeLeft.includes(frameCount)){
-      face[1].resize(339 * 0.5,409 * 0.5)
-      image(face[1],521,227);
+      uu[1].resize(339 * 0.5,409 * 0.5)
+      image(uu[1],521,227);
 
     }
     if(eyeRight.includes(frameCount)){
-      face[0].resize(339 * 0.5,409 * 0.5)
-      image(face[0],96,337);
+      uu[0].resize(339 * 0.5,409 * 0.5)
+      image(uu[0],96,337);
 
     }
     if(mouth.includes(frameCount)){
-      face[2].resize(669 * 0.5,299 * 0.5)
-      image(face[2],288,557);
+      uu[2].resize(669 * 0.5,299 * 0.5)
+      image(uu[2],288,557);
 
     }
     if(reset.includes(frameCount)) {
       background(200,200,200);
     }
+  pop()
+//LOUIS HELPED ME WITH TOP PART "INCLUDES"
+image(loading,540,400);
+//LOADING FACE ANIMATION AFTER
+ 
+  if(frameCount >= 80){
 
-
-}
-
-
-//  for(let i = 0; i < face.length; i++){
-  //  image(face[i], i*200, 227, 339/2, 409/2); // (variable for the image, x, y, w, h)
-    //}
-
-//bullion use flag in set up, modulo function
-
-  /* THIS IS WHEN IT IS DONE LOADING
-
-    if(frameCount >= 500) {
-    fill(200,0,0);
-    triangle(150, 250, 250, 250, 200, 150);
-    textSize(50);
-    fill(0);
-    text('Done Loading', 15, 300);
+    image(back, 540, 960);
+    image(user,540,440);
+    back.volume(0);
+    back.loop();
+    back.hide();
+//LOOP STARTS FOR ALTERNATION SHIRTS
+    image(shirts[2],540,1300);
   }
-  */
+
+  if (count % 3 == 1){
+    image(shirts[1],540,1300);
+  } else if(count % 3 == 2){
+    image(shirts[0], 540, 1300);
+  }
+//LOOP ENDS FOR ALTERNATION SHIRTS
+
+//SCAN LINE
+  if(frameCount >= 100){
+    image(beam[0],540,y+1626);
+    y = y - 2
+
+  }
+  if (y < -710){
+    y = + 0
+    count++;
+    print(count);
+
+  }
+  print(y);
+}
